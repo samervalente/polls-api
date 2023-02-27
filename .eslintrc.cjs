f03ec3105bd91e12ed164fd92502bc5461a6010a
samervalente <samervalente@gmail.com>
2023-02-27 17:54:31 -0300

    add v2 huskty and lint staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['standard-with-typescript'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
-  env:{
-    node:true
+  env: {
+    node: true
   }
-};
+}
--- index.ts
+++
@@ -1,5 +0,0 @@
-function printMessage (text: string): void {
-  console.log(text + 'world')
-}
-
-printMessage("hello")
