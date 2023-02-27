8d72edd0f3cef1cffce4bc0727e0d0234ffea20e
samervalente <samervalente@gmail.com>
2023-02-27 19:50:49 -0300

    trying to fix lint-stage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ['standard-with-typescript'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
-  env: {
-    node: true
+  env:{
+    node:true
   }
 }
