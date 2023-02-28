914a43d71cfe2b886ed7623df2c0e252ceb5a778
samervalente <samervalente@gmail.com>
2023-02-28 17:17:24 -0300

    feat: ensure SignUpController return error 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,28 @@
-interface IHttpRequest {
-  body: {
-    email: string;
-    password: string;
-    passwordConfirmation: string;
-  };
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface IHttpRequest {
+  body: IUser;
 }
 
 export class SignupController {
-  handle(httpRequest: IHttpRequest): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
-    };
+  handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      };
+    }
   }
 }
