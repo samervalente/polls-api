ea2aa02e6717554fd91d7d71b5410086c4316b26
samervalente <samervalente@gmail.com>
2023-02-28 17:24:26 -0300

    test: ensure SignUpController return error 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,16 +1,39 @@
-import { SignupController } from "./signup"
+import { IHttpRequest, SignupController } from "./signup"
+
+
+type RecursivePartial<T> = {
+    [P in keyof T]?:
+      T[P] extends (infer U)[] ? RecursivePartial<U>[] :
+      T[P] extends object ? RecursivePartial<T[P]> :
+      T[P];
+  };
 
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
         const sut = new SignupController()
-        const httpRequest = {
+        const httpRequest: RecursivePartial<IHttpRequest> = {
             body:{
-                email:'faker_email@mail.com',
-                password:'faker_password',
-                passwordConfirmation:'faker_password'
+                email:'fake_email@mail.com',
+                password:'fake_password',
+                passwordConfirmation:'fake_password'
             }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error("Missing param: name"))
+    })
+
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignupController()
+        const httpRequest = {
+            body:{
+                name:'fake name',
+                password:'fake_password',
+                passwordConfirmation:'fake_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error("Missing param: email"))
     })
 })
