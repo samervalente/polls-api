ac4aeed8c68f167c48c338c0f97474be1beae8a1
samervalente <samervalente@gmail.com>
2023-03-08 11:41:58 -0300

    feat: ensure SignUpController.handle returns 500 if EmailValidator throw new exception

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,35 +1,43 @@
 import { IHttpRequest, IHttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, internalServerError } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamError } from '../errors/invalid-param-error';
+import { ServerError } from '../errors/server-error';
 
 export class SignupController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  private readonly emailValidator: EmailValidator;
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
 
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = [
-      'email',
-      'name',
-      'password',
-      'passwordConfirmation'
-    ];
+    try {
+      const requiredFields = [
+        'email',
+        'name',
+        'password',
+        'passwordConfirmation'
+      ];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValidEmail) {
-      return badRequest(new InvalidParamError('email'));
-    }
+      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValidEmail) {
+        return badRequest(new InvalidParamError('email'));
+      }
 
-    return {
-      statusCode: 200,
-      body: 'User registered successfully!'
-    };
+      return {
+        statusCode: 200,
+        body: 'User registered successfully!'
+      };
+    } catch (error) {
+      return internalServerError(new ServerError());
+    }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,8 @@ export const badRequest = (error: Error): IHttpResponse => ({
   statusCode: 400,
   body: error
 });
+
+export const internalServerError = (error: Error): IHttpResponse => ({
+  statusCode: 500,
+  body: error
+});
