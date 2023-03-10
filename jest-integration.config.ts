6bc63d63df60e9307d74126ed87397d84245ecf2
samervalente <samervalente@gmail.com>
2023-03-10 13:10:42 -0300

    chore: adjust jest config import statement and staged test script related tests source path

---
+++ jest-integration.config.ts
@@ -0,0 +1,3 @@
+import config from './jest.config.js';
+config.testMatch = ['**/*.test.ts'];
+export default config;
---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const config = {
+  roots: ['<rootDir>/src'],
+  //collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: "node",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
+
+module.exports = config
