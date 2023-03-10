11e9f2da656f76a389995ba72eb8756ce57ba04a
samervalente <samervalente@gmail.com>
2023-03-10 09:18:27 -0300

    feat: ensure SignupController returns 400 if passwordConfirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -29,6 +29,9 @@ export class SignupController implements Controller {
         return badRequest(new InvalidParamError('email'));
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
       return {
         statusCode: 200,
         body: 'User registered successfully!'
