650655dcee303ca9345e867e5d36e7ab7c8ce0c2
samervalente <samervalente@gmail.com>
2023-03-13 11:00:46 -0300

    feat: ensure EmailValidatorAdapter returns false if EmailValidator returns false

--- src/domain/use-cases/add-account.ts
+++ src/domain/use-cases/add-account.ts
@@ -2,5 +2,10 @@ import { Account } from '../../entities/account';
 import { AccountModel } from '../models/account';
 
 export abstract class AddAccount {
-  abstract add(account: AccountModel): Promise<Account>;
+  abstract add(account: AccountModel): Promise<Account>; //add function signature
+}
+
+//or
+export interface AddAccountInterface {
+  add(account: AccountModel): Promise<Account>; //add function signature
 }
