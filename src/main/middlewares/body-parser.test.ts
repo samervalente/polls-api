3feb6196ab05e0317501a3223a24ea8ac0001a74
samervalente <samervalente@gmail.com>
2023-03-23 11:30:25 -0300

    test: ensure body parser middleware converts http request body to json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,17 @@
+import { response } from 'express';
+import request from 'supertest';
+import app from '../config/app';
+
+describe('Body parser middleware', () => {
+  test('Should parse body type to json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body);
+    });
+
+    const bodyRequest = { name: 'Samer' };
+    await request(app)
+      .post('/test_body_parser')
+      .send(bodyRequest)
+      .expect(bodyRequest);
+  });
+});
